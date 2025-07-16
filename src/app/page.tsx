0cc0f64751cc9a31397bb8dc39d3061029afca01
hexendevelopers <hexenwebcreators@gmail.com>
2025-07-16 13:00:54 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Image from "next/image";
-
+import { useRouter } from "next/navigation";
+import { userAgentFromString } from "next/server";
+import { useEffect } from "react";
 export default function Home() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/admin/login");
+  }, []);
+
+  return <div className=""></div>;
 }
