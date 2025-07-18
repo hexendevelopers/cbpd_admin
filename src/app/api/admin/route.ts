1a0d94696d463c967b1eb6c3404d43cf402d434f
hexendevelopers <hexenwebcreators@gmail.com>
2025-07-18 15:20:34 +0530

    once again

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import connectDB from "@/configs/mongodb";
 import Admin from "@/models/adminModel";
 import { verifyAdminToken } from "@/lib/verifyToken";
