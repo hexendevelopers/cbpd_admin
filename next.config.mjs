5913c7c6d7edfc0fac43fbdf04ca039d820a89e7
hexendevelopers <hexenwebcreators@gmail.com>
2025-07-21 16:46:16 +0530

    newone

--- next.config.mjs
+++ next.config.mjs
@@ -3,31 +3,31 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       headers: [
-  //         {
-  //           key: "Access-Control-Allow-Origin",
-  //           value: "https://cbpd.co.uk",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,POST,PUT,DELETE,OPTIONS",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value: "Content-Type, Authorization",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Credentials",
-  //           value: "true",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://cbpd-new-version.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
